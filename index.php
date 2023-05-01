38e988d526f9abc2f1b9c534ceb563615573e5af
Steve Grogorick <steve@grogorick.de>
2023-05-01 16:02:55 +0200

    bugfix broken upload

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require('localization/localization.php');
 LOCALIZATION\INIT_FROM_FILE('localization.yaml');
-LOCALIZATION\SET_LOCALE($_GET['lang']);
+LOCALIZATION\SET_LOCALE($_GET['lang'] ?? null);
 use function LOCALIZATION\L;
 
 require('utils.php');
