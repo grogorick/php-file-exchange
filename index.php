650a5ad922c65faaa62035c0979261055d7303d6
Steve Grogorick <steve@grogorick.de>
2023-05-05 16:55:19 +0200

    do not try to create the upload dir if it is not found; refactor index.php

--- index.php
+++ index.php
@@ -4,19 +4,27 @@ LOCALIZATION\INIT_FROM_FILE('localization.yaml');
 LOCALIZATION\SET_LOCALE($_GET['lang'] ?? null);
 use function LOCALIZATION\L;
 
-$dir = $_GET['dir'] ?? './FILES/';
+
+$dir = $_GET['dir'] ?? 'FILES';
 if (!in_array(mb_substr($dir, -1), ['\\', '/']))
-  $dir += '/';
+  $dir .= '/';
 define('DIR', $dir);
+
+if (!is_dir(DIR))
+  die(L('precheck_directory_not_found', DIR));
+if (!is_writable(DIR))
+  die(L('precheck_directory_not_writable', DIR));
 
 define('FILES', array_filter(scandir(DIR), fn($file) => is_file(DIR . $file)));
 
-require('php/utils.php');
 
 $allowed_file_extensions = []; // ['.jpg', '.png', '.zip'];
 $prohibited_file_extensions = ['.htm', '.pdf'];
 $max_file_size = null; // '10M';
 $disk_quota = '100M';
+
+
+require('php/utils.php');
 
 if (is_null($max_file_size))
   $max_file_size = parse_file_size(ini_get('upload_max_filesize'));
@@ -25,12 +33,6 @@ else
 
 $disk_quota = parse_file_size($disk_quota);
 
-if (!is_dir(DIR)) {
-  if (file_exists((DIR)))
-    die(L('precheck_path_is_file', DIR));
-  elseif (!mkdir(DIR, 0777, true))
-    die(L('precheck_mkdir_failed', DIR));
-}
 
 require('php/actions.php');
 require('php/view.php');
