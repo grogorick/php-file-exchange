e96f4993fcdc2f80d640ed209839ef369c6e38a6
Steve Grogorick <steve@grogorick.de>
2023-05-08 21:21:52 +0200

    restrict access to admin page with password

--- admin/index.php
+++ admin/index.php
@@ -3,6 +3,12 @@ require('../localization/localization.php');
 LOCALIZATION\INIT_FROM_FILE('localization.yaml');
 LOCALIZATION\SET_LOCALE($_GET['lang'] ?? null);
 
+require('php/auth_pw.php');
+if (!Auth::is_logged_in()) {
+  Auth::handle_login();
+  exit();
+}
+
 require('../php/config.php');
 DirectoryConfig::load('../');
 
