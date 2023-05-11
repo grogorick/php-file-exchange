e20d6ca7f502028749ff9ce5ee3a32a6c9912ac8
Steve Grogorick <steve@grogorick.de>
2023-05-11 20:14:11 +0200

    skip dropped directories

--- js/upload_dragndrop.js
+++ js/upload_dragndrop.js
@@ -12,10 +12,32 @@ on(document, 'dragover', e =>
   startTimeoutToCancelDragDrop();
 });
 
-on(document, 'drop', e =>
+on(document, 'drop', async e =>
 {
   dragDropIndicator.classList.add('hidden');
   cleanupPreviousUploadItems();
-  if (prepareFilesForUpload(e.dataTransfer.files))
+
+  let fileList = [];
+  const supportsFileSystemAccessAPI = 'getAsFileSystemHandle' in DataTransferItem.prototype;
+  const supportsWebkitGetAsEntry = 'webkitGetAsEntry' in DataTransferItem.prototype;
+  if (supportsFileSystemAccessAPI || supportsWebkitGetAsEntry) {
+    const fileHandlesPromises = [...e.dataTransfer.items]
+      .filter(item => item.kind === 'file')
+      .map(item =>
+        supportsFileSystemAccessAPI
+          ? item.getAsFileSystemHandle()
+          : item.webkitGetAsEntry(),
+      );
+    for await (const fileHandle of fileHandlesPromises) {
+      if (fileHandle.kind === 'file' || fileHandle.isFile)
+        fileList.push(await fileHandle.getFile());
+      else
+        console.log('Skip directory:', fileHandle.name);
+    }
+  }
+  else
+    fileList = e.dataTransfer.files;
+
+  if (prepareFilesForUpload(fileList))
     uploadFiles();
 });
