927fa0720bad3700773c95335050a8ceecfc7264
Steve Grogorick <steve@grogorick.de>
2023-05-11 22:13:14 +0200

    add directory name to title

--- php/view.php
+++ php/view.php
@@ -7,7 +7,7 @@ use function LOCALIZATION\L;
   <meta charset="utf-8">
   <meta name="robots" content="noindex,nofollow" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>File Exchange</title>
+  <title><?=$dir_name?> &#183; File Exchange</title>
   <link rel="icon" type="image/png" href="favicon.png" />
   <link rel="stylesheet" href="css/style.css">
 </head>
